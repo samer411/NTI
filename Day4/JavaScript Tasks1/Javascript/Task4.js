3038761de5c208bb4a43e6aeb8d520bc0593d7bf
Samer411 <samer.nassim.behery@gmail.com>
2026-05-04 20:03:40 +0300

    Update vowel counting logic and enhance Task5.js functionality; modify index.html to include Task5.js

--- Day4/JavaScript Tasks1/Javascript/Task4.js
+++ Day4/JavaScript Tasks1/Javascript/Task4.js
@@ -2,17 +2,14 @@ var word = prompt("Enter Your Text, Please")
 var vowels ="aeiou";
 if(isNaN(word) && word.trim()!==""){
     var new_word = "";
+    var counter = 0;
     for(var i=0;i<word.length;i++){
-        if(vowels.lastIndexOf(word[i] )=== -1){
-            new_word+=word[i];
+        if(vowels.lastIndexOf(word[i] )!== -1){
+            counter++;
+
         }
     }
-    if(new_word.length==word.length){
-    alert("Success")
-}
-else{
-    alert("Failure")
-}
+    alert("Number of vowels is: "+counter)
 }
 else{
     alert("Enter A Valid Text")
--- Day4/JavaScript Tasks1/Javascript/Task5.js
+++ Day4/JavaScript Tasks1/Javascript/Task5.js
@@ -3,16 +3,16 @@ var word = prompt("Enter Your Text, Please")
 if(word.trim()!==""){
     var new_word = "";
     for(var i=0;i<word.length;i++){
-        if(vowels.lastIndexOf(word[i] )=== -1){
+        console.log(word[i].charCodeAt(0))
+        if(
+            (word[i].charCodeAt(0)<123 && word[i].charCodeAt(0)>96)||// Lowercase 
+            (word[i].charCodeAt(0)<91 && word[i].charCodeAt(0)>64)|| // Uppercase
+            (word[i].charCodeAt(0)<58 && word[i].charCodeAt(0)>47)   // Numbers
+        ){
             new_word+=word[i];
         }
     }
-    if(new_word.length==word.length){
-    alert("Success")
-}
-else{
-    alert("Failure")
-}
+    alert("The New Word is: "+new_word)
 }
 else{
     alert("Enter A Valid Text")
