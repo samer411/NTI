5283eefba019e34c7fe07f1be3d3dacb6c0999dd
Samer411 <samer.nassim.behery@gmail.com>
2026-05-04 18:55:41 +0300

    Fix condition check and improve logging in text manipulation script

--- Day4/JavaScript Tasks1/Javascript/Task1.js
+++ Day4/JavaScript Tasks1/Javascript/Task1.js
@@ -2,13 +2,13 @@ var text = prompt("Enter Your Text, Please")
 var old_words = text.split(" ");
 var words=[];
 for(var i=0;i<old_words.length;i++){
-    // console.log(old_words[i][0]);
-    if(typeof(parseInt(old_words[i][0]))!=='number'){
+    console.log(old_words[i][0]);
+    if(isNaN(parseInt(old_words[i][0]))!==false){
         words.push(old_words[i])
     }
 }
 console.log(words)
-if(isNaN(text)&& text.trim()!==""){
+if(isNaN(text) && text.trim()!==""){
     var new_text = "";
     for(var i=0;i<words.length;i++){
         var new_capital_letter = words[i][0].toUpperCase();
