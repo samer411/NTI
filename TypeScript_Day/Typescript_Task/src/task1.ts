6efb68c276dda49e22027915c431895f55530cdf
Samer Nassim <samer.nassim.behery@gmail.com>
2026-05-18 15:26:41 +0300

    feat: implement function to log words starting with consonants from a given string

--- TypeScript_Day/Typescript_Task/src/task1.ts
+++ TypeScript_Day/Typescript_Task/src/task1.ts
@@ -1,3 +1,14 @@
-function Return_If_The_First_Character_Vowel(){
+function Return_If_The_First_Character_Vowel(text:string){
+    let arr:string[] = text.split(" ");
+    let vowels:string = "aeiou"
+    for( let word of arr){
+
+        // if(!vowels.includes(word.charAt(0).toLowerCase())){
+        if(!vowels.includes(word[0]!.toLowerCase())){
+            console.log(word)
+        }
+    }
     
 }
+let str:string = "Apple Banana Cat"
+Return_If_The_First_Character_Vowel(str)
