6b185ce20f3ac7317474de22a07f4bec78552c13
Samer Nassim <samer.nassim.behery@gmail.com>
2026-05-18 23:39:19 +0300

    Add Tailwind CSS setup and initial HTML structure for projects
    - Created package.json for Tailwind CSS dependencies.
    - Added index.html with a basic structure and dark mode toggle functionality.
    - Included input.css and output.css for Tailwind CSS styles.
    - Implemented a sample SVG icon for TypeScript project.
    - Developed index.html for TypeScript project with a responsive menu and dark mode toggle.
    - Created input.css and output.css for TypeScript project styles.
    - Added TypeScript functionality to fetch and display meal data from an API.

---
+++ TypeScript_Day/Typescript_Task/src/task2.ts
@@ -0,0 +1,32 @@
+import nodeUrl = require("node:url");
+
+type Meal = {
+    idMeal : string;
+    strMeal : string;
+    strMealThumb : string;
+}
+
+type ApiResponse={
+    meals:Meal[];
+}
+
+const main = document.getElementsByTagName("main")[0]
+fetch("https://www.themealdb.com/api/json/v1/1/filter.php?i=")
+.then((x)=>x.json())
+.then((data)=>{
+    for(let meal of data.meals){
+        console.log("samer")
+       main!.innerHTML+=`
+                <div class="dark:bg-transparent bg-white m-3 rounded shadow-lg  w-[400px] h-full p-3">
+                <img class="w-full border-solid border-2 border-gray-500" src="${meal.strMealThumb}" alt="">
+                <p class="mt-3">${meal.strMeal}</p>
+            </div>
+    ` 
+    }
+    
+})
+
+function draw(data:ApiResponse){
+    // console.log(data.meals)
+}
+
