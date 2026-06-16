a20dc38bdb2edea16d571e79a03383b4f818e29c
Samer Nassim <samer.nassim.behery@gmail.com>
2026-06-16 10:50:03 +0300

    chore: initialize package.json with dependencies for Express, MSSQL, and Sequelize

---
+++ Back-End/My_SQL_Server_Day4/migrations/20260616073743-create-user.js
@@ -0,0 +1,32 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      // string is translated to nvarchar(255) every char is byte
+      name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
